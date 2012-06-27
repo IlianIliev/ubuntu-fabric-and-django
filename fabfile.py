3fcd8afa73cd151e4e34942027b1f34b1c7cf7ae
ilian <ilian@ilian-N76VZ.(none)>
2012-06-28 00:43:55 +0300

    Bit of refactory

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,15 @@
-import re, sys, os
+import re, sys, os, inspect
 
 from fabric.api import local, run, sudo, prompt
 from fabric.context_managers import lcd, prefix
 
-from db import AVAILABLE_DB_MODULES
+from db import select_db_engine
 from db.mysql import setup_db_server, create_db_and_user
 
 from utils import generate_password, add_os_package
 
+
+FABFILE_LOCATION = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
 DIRECTORY_NAME_REGEXP = r'^[a-zA-Z_].[\w_-]+$'
 
@@ -25,34 +27,13 @@ REQUIRED_SYSTEM_PACKAGES = [
 ]
 
 
-def setup_server():
-    # install system packages
-    #add_os_package(' '.join(REQUIRED_SYSTEM_PACKAGES))
-    setup_db_server()
-
-
-def setup_db(name):
-    text = ['Before setting up database you must select its type']
-    [text.append('%s [%s]' % pair) for pair in AVAILABLE_DB_MODULES]
-    text.append('Please enter your choice:')
-    val = r'^(%s)$' %  '|'.join([pair[1] for pair in AVAILABLE_DB_MODULES])
-    db_package_name = prompt('\n'.join(text), validate=val, default=AVAILABLE_DB_MODULES[0][1])
-    db_package = __import__('db.%s' % db_package_name, fromlist=['create_db_and_user'])
-    db_package.create_db_and_user(name)
-
-
-
-"""
-This fabric script automates the creation of a virtual environment and a Django
-project. The result will be virtual environtment with the name of the project.
-The folder namer where the project code will be placed is specified in
-SOURCE_DIRECTORY_NAME, a static root folder will be created and settings.py
-will be updated.
-"""
-
-
+# Start project part
 def create_virtual_env():
     local('virtualenv --no-site-packages .')
+
+
+def install_packages():
+    local('pip install -r required_packages.txt')
 
 
 def create_project_directory(name):
@@ -67,30 +48,46 @@ def create_project_directory(name):
         local('mkdir %s' % name)
 
 
-def install_packages():
-    local('pip install -r required_packages.txt')
-
-
 def create_django_project(name):
     local('mkdir %s' % SOURCE_DIRECTORY_NAME)
     local('mkdir static')
     local('mkdir media')
-    local('python ./bin/django-admin.py startproject --template "./project_template/" %s %s' % (name, SOURCE_DIRECTORY_NAME))
+    local('python ./bin/django-admin.py startproject --template "%s" %s %s' % (os.path.join(FABFILE_LOCATION, 'project_template/'), name, SOURCE_DIRECTORY_NAME))
 
 
-def start_project(name=None):
-    import inspect, os
-    print os.path.dirname(inspect.getfile(inspect.currentframe()))
-    exit()
-
+def startproject(name=None):
     create_project_directory(name)
-    local('cp required_packages.txt %s' % name)
+    
     with lcd(name):
         create_virtual_env()
+        local('cp %s .' % os.path.join(FABFILE_LOCATION, 'required_packages.txt'))
         ve_activate_prefix = os.path.join(os.getcwd(), name, 'bin', 'activate')
         with prefix('. %s' % ve_activate_prefix):
             install_packages()
             create_django_project(name)
-            create_db_and_user()
+            with select_db_engine as db_engine:
+                create_db_and_user(name, local=True)
+            update_settings()
+            copy_fabfiles()
             manage_py_path = os.path.join(SOURCE_DIRECTORY_NAME, 'manage.py')
             local('python %s collectstatic' % manage_py_path)
+
+
+# Server setup
+def setup_server():
+    # install system packages
+    #add_os_package(' '.join(REQUIRED_SYSTEM_PACKAGES))
+    setup_db_server()
+
+
+def setup_db(name):
+    select_db_engine()
+    return
+    text = ['Before setting up database you must select its type']
+    [text.append('%s [%s]' % pair) for pair in AVAILABLE_DB_MODULES]
+    text.append('Please enter your choice:')
+    val = r'^(%s)$' %  '|'.join([pair[1] for pair in AVAILABLE_DB_MODULES])
+    db_package_name = prompt('\n'.join(text), validate=val, default=AVAILABLE_DB_MODULES[0][1])  
+    db_package = __import__('db.%s' % db_package_name, fromlist=['create_db_and_user'])
+    db_package.create_db_and_user(name)
+
